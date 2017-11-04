180e3a5e4048c65945171bf270713a2e70b9c43a
lyckan <callejasmartinez12@gmail.com>
2017-11-03 23:03:09 -0600

    Cambio a FCM

--- app/src/main/java/sv/com/tecnocompu/tecnocompuapp/notification/NotificacionsListenerService.java
+++
@@ -1,7 +0,0 @@
-package sv.com.tecnocompu.tecnocompuapp.notification;
-
-import com.google.android.gms.gcm.GcmListenerService;
-
-
-public class NotificacionsListenerService extends GcmListenerService {
-}
---
+++ app/src/main/java/sv/com/tecnocompu/tecnocompuapp/notification/TecnocompuFirebaseInstanceIdService.java
@@ -0,0 +1,8 @@
+package sv.com.tecnocompu.tecnocompuapp.notification;
+
+/**
+ * Created by cesar on 11-03-17.
+ */
+
+public class TecnocompuFirebaseInstanceIdService {
+}
